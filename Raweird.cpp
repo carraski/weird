c20d7478abbe4edd1fd5c7e55aa699b8e372830b
carraski <carrasquerrur@ieselaios.com>
2026-02-19 08:59:30 +0000

    empiezo el codigo

---
+++ Raweird.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+//si un numero n es par lo divide entre dos
+//si ese numero n es impar lo multiplica x3 y le suma 1
+//se tiene que repetir la secuencia hasta que se llegue a 1
+int n;
