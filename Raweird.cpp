bf5c259bc4806a9843dace52e32c97bb525c7ca5
carraski <carrasquerrur@ieselaios.com>
2026-02-20 09:53:32 +0000

    Completado el Weird

--- Raweird.cpp
+++ Raweird.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 using namespace std;
+int main(){
+//mirar el ejercicio en CSES - weird Algorithm
 //si un numero n es par lo divide entre dos
 //si ese numero n es impar lo multiplica x3 y le suma 1
 //se tiene que repetir la secuencia hasta que se llegue a 1
 int n;
+    cout << "Dime que numero quieres convertir en 1 ";
+   
+    cin >> n;
+    // 0 es un numero par
+// 1 es para un numero impar
+do{
+if (n%2 == 0){
+    //los dos = es para que sea una comparacion
+n= n/2 ;
+cout << "el numero ahora es " <<n<< "\n";
+}
+else {
+n=n*3;
+n=n+1;
+cout << "el numero ahora es " <<n<< "\n";
+}
+}
+while (n != 1);
+//n! sirve para decir que algo no es igual
+}
